32a4a1b2d4d141e55b7996000361ba97205df0ca
Kevin Carpenter <carpenter.kevin@gmail.com>
2022-08-29 13:39:53 -0700

    Update Callout.server.tsx

--- src/components/modules/Callout.server.tsx
+++ src/components/modules/Callout.server.tsx
@@ -11,11 +11,11 @@ export default function CalloutModule({colorTheme, module}: Props) {
   return (
     <div className={module.klass} style={{color: colorTheme?.text}}>
       {/* Text */}
-      <div className={clsx('cta-block mx-auto max-w-[60rem] pb-16 pt-16')}>
+      <div className={clsx('cta-block mx-auto max-w-[60rem] py-16')}>
         <h1 className="pb-4 pt-4 text-center text-2xl font-bold text-lightGray">
           {module.title}
         </h1>
-        <p className="pl-12 pr-12 pb-4 text-center text-lightGray ">
+        <p className="px-12 pb-4 text-center text-lightGray ">
           {module.text}
         </p>
       </div>
